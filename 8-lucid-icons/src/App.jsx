093c0e6d643c33ed688747b1ca7c604ac5f78850
Deepti <2100deepti@gmail.com>
2024-09-12 17:48:00 +0530

    8-lucide-icon and lab

---
+++ 8-lucid-icons/src/App.jsx
@@ -0,0 +1,13 @@
+import {Camera} from "lucide-react"
+
+function App() {
+ 
+  return (
+    <>
+     <h1>Placeholder</h1>
+     <Camera color="cyan" size={48} />
+    </>
+  )
+}
+
+export default App
